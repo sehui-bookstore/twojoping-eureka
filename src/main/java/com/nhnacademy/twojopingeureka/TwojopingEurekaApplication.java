535cd782f2bf427dbd02f59ab0756b5cae25734e
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-10-24 17:44:33 +0900

    feat: init project

---
+++ src/main/java/com/nhnacademy/twojopingeureka/TwojopingEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.twojopingeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwojopingEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwojopingEurekaApplication.class, args);
+    }
+
+}

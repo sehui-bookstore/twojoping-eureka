69ff339908fa777dfaf9fc46f5b45a531c0c01cb
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-11-02 08:58:14 +0900

    docs(SecurityConfig.java): Javadoc 문서형 주석 작성

--- src/main/java/com/nhnacademy/twojopingeureka/config/SecurityConfig.java
+++ src/main/java/com/nhnacademy/twojopingeureka/config/SecurityConfig.java
@@ -9,10 +9,26 @@ import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+/**
+ * Spring Security를 위한 설정 클래스입니다.
+ * <p>
+ *     이 클래스는 HTTP 보안을 설정하고, 모든 요청에 대해 인증을 요구합니다.
+ * </p>
+ *
+ * @author : 정세희
+ * @date : 2024-10-25
+ */
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
+    /**
+     * 보안 필터 체인을 구성합니다.
+     *
+     * @param http HttpSecurity 객체로, 보안 설정을 구성하는 데 사용됩니다.
+     * @return 구성된 SecurityFilterChain 객체
+     * @throws Exception 보안 설정 중 발생할 수 있는 예외
+     */
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
